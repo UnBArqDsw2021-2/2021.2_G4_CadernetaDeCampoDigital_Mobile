dd105029a378c273bf39a7bb956d48e0b5a90e90
carlosFiuza <carlosfiuza1234@gmail.com>
2022-03-29 18:19:58 -0300

    #160 Criando PropertyPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:caderneta_campo_digital/pages/property/property_page.dart';
 import 'package:flutter/material.dart';
-import 'package:caderneta_campo_digital/pages/splash_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: SplashScreenPage(),
+      home: PropertyPage(),
     );
   }
 }
