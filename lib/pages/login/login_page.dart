12ca9e6e0317274a9246b2d09f5a8cc49cf3dc84
Denniel William <dennielwilliam.unb@gmail.com>
2022-03-31 17:49:50 -0300

    Resolvendo bug na login page
    Co-authored-by: brenno-silva <brenno.silva037@gmail.com>

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -8,8 +8,6 @@ import 'package:caderneta_campo_digital/global/global.dart';
 import 'package:caderneta_campo_digital/pages/home_produtor/home_produtor.dart';
 import 'package:caderneta_campo_digital/pages/home_tecnico/home_tecnico.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:caderneta_campo_digital/pages/select_account/select_account.dart';
 import 'package:caderneta_campo_digital/utils/utils.dart';
 import 'package:caderneta_campo_digital/components/underline_button.dart';
@@ -74,139 +72,11 @@ class _LoginPageState extends State<LoginPage> {
                         size: 14,
                         color: Colors.red,
                       ),
-                      Image.asset("assets/logo.png"),
-                      Form(
-                        key: _formKey,
-                        child: Column(
-                          children: [
-                            Container(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: size.height * 0.025,
-                                vertical: size.width * 0.01,
-                              ),
-                              child: TextFormField(
-                                key: Key("cpfField"),
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  labelText: 'CPF',
-                                ),
-                                validator: (value) =>
-                                    loginController.validateCpf(value),
-                                controller: loginController.controllerCpf,
-                                keyboardType: TextInputType.number,
-                                inputFormatters: [
-                                  LengthLimitingTextInputFormatter(14),
-                                  MaskTextInputFormatter(
-                                    mask: "###.###.###-##",
-                                  ),
-                                ],
-                              ),
-                            ),
-                            Container(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: size.height * 0.025,
-                                vertical: size.width * 0.01,
-                              ),
-                              child: TextFormField(
-                                key: Key("passwordField"),
-                                obscureText: obscurePassword,
-                                validator: (value) =>
-                                    loginController.validatePassword(value),
-                                controller: loginController.controllerPassword,
-                                decoration: InputDecoration(
-                                  border: OutlineInputBorder(),
-                                  suffixIcon: IconButton(
-                                    icon: Icon(Icons.remove_red_eye_outlined),
-                                    onPressed: () {
-                                      setState(() {
-                                        obscurePassword = !obscurePassword;
-                                      });
-                                    },
-                                    highlightColor: Colors.transparent,
-                                    splashColor: Colors.blue.withOpacity(0.2),
-                                  ),
-                                  labelText: 'Senha',
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      failedLogin
-                          ? Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Icon(
-                                  Icons.cancel_outlined,
-                                  size: 14,
-                                  color: Colors.red,
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(left: 6),
-                                  child: Text(
-                                    loginController.errorText,
-                                    style: TextStyle(color: Colors.red),
-                                  ),
-                                ),
-                              ],
-                            )
-                          : Container(),
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: size.height * 0.025),
-                        child: MaterialButton(
-                          onPressed: buttonPressed,
-                          highlightColor: Colors.transparent,
-                          splashColor: Colors.transparent,
-                          child: Container(
-                            height: size.height * 0.07,
-                            width: size.width * 0.5,
-                            decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              color: Color(0XFF00B4D8),
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Entrar",
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: size.height * 0.01),
-                        child: MaterialButton(
-                          // ignore: no-empty-block
-                          onPressed: () {
-                            navigateToSelectAccount();
-                          },
-                          splashColor: Color(0XFF00B4D8).withOpacity(0.2),
-                          highlightColor: Colors.transparent,
-                          child: Container(
-                            height: size.height * 0.07,
-                            width: size.width * 0.5,
-                            decoration: BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15)),
-                              color: Colors.transparent,
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Cadastro",
-                                style: TextStyle(
-                                  color: Color(0XFF00B4D8),
-                                  fontSize: 20,
-                                  decoration: TextDecoration.underline,
-                                ),
-                              ),
-                            ),
-                          ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 6),
+                        child: Text(
+                          loginController.errorText,
+                          style: TextStyle(color: Colors.red),
                         ),
                       ),
                     ],
