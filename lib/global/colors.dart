ddfa1caddaad2965fddbf0284995f1872cd05625
PauloAbiAcl <pv.silva23@hotmail.com>
2022-04-05 14:19:09 -0300

    #165 Pequenas correcoes

--- lib/global/colors.dart
+++ lib/global/colors.dart
@@ -4,5 +4,6 @@ import 'package:flutter/cupertino.dart';
 class MyColors {
   final blueButton = const Color(0xFF00B4D8);
   final white = const Color(0xFFFFFFFF);
+  final darkBlue = const Color(0XFF03045E);
   const MyColors();
 }
